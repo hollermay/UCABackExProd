5b6054a9bbccb3041dfcd3758eee184844c17fdd
hollermay <udayanmoudgil@gmail.com>
2024-11-25 23:33:42 +0530

    added new libraries and added new crud operations

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,35 +1,137 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState,useEffect } from 'react'
+import axios from 'axios'
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+  const[notes, setNotes] = useState([]);
+  const[create,setCreate] = useState({
+    title: "",
+    body: "",
+  });
+  const[update,setUpdate] = useState({
+    _id:null,
+    title: "",
+    body: "",
+  });
+  
+  useEffect(() => {
+    fetchNotes();
+  }, []);
+
+  const fetchNotes = async () => {
+    const res = await axios.get('http://localhost:3000/notes')
+    setNotes(res.data.notes);
+    console.log(res);
+  };
+
+  const updateForm = (e) => {
+    const{name, value} = e.target;
+
+    setCreate({
+      ...create,
+      [name]: value,
+    });
+  }
+
+  const createNote = async (e) => {
+    e.preventDefault();
+    const res = await axios.post('http://localhost:3000/notes', create);
+    console.log(res);
+
+    setNotes([...notes, res.data.note]);
+
+    setCreate({
+      title: "",
+      body: "",
+    });
+  }
+
+  const deleteNote = async (_id) => {
+    const res = await axios.delete(`http://localhost:3000/notes/${_id}`);
+    console.log(res);
+
+    const newNotes = notes.filter((note) => note._id !== _id);
+    setNotes(newNotes);
+  }
+
+  const handleUpdate = (e) => {
+    const{name, value} = e.target;
+
+    setUpdate({
+      ...update,
+      [name]: value,
+    });
+  }
+
+  const toggleUpdate = (note) => {
+    // get current note values
+    setUpdate({title: note.title, body: note.body, _id: note._id} );
+    //update the stare
+  }
+
+  const updateNote = async () => {
+    try {
+      const { title, body } = update;
+  
+      const res = await axios.put(`http://localhost:3000/notes/${update._id}`, { title, body });
+  
+      const newNotes = [...notes];
+      const index = newNotes.findIndex((note) => note._id === update._id);
+      console.log(res);
+      newNotes[index] = res.data.note;
+      setNotes(newNotes);
+  
+      setUpdate({
+        _id: null,
+        title: "",
+        body: "",
+      });
+    } catch (error) {
+      console.error("Error updating note:", error);
+    }
+  };
+  
+
 
   return (
     <>
       <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <h2>Notes:</h2>
+      <ul>
+        {notes.map((note) => (
+          <li key={note._id}>
+            <h3>{note.title}</h3>
+            <p>{note.body}</p>
+            <button onClick={() => deleteNote(note._id)}> Delete</button>
+            <button onClick={() => toggleUpdate(note)}>Update</button>
+          </li>
+        ))}
+      </ul>
+
+
+      </div> 
+
+      {update._id &&  (<div>
+        <h2>Update Note</h2>
+        <form onSubmit={updateNote}>
+          <input onChange={handleUpdate} value={update.title} name='title' type="text" placeholder="Title" />
+          <textarea onChange={handleUpdate} value={update.body} name='body' placeholder="Body" />
+          <button type="submit">Update Note</button>
+        </form>
+      </div>)}
+
+      <div>
+        <h2> Create Note</h2>
+        <form onSubmit={createNote}>
+          <input onChange={updateForm} value={create.title} name='title' type="text" placeholder="Title" />
+          <textarea onChange={updateForm} value={create.body} name='body' placeholder="Body" />
+          <button type='submit'>Submit</button>
+        </form>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+
     </>
   )
 }
 
-export default App
+export default App;
